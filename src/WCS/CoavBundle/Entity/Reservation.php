39b02b2a5ca25c8818b99a0d3b57968141f4dff9
Veronique Monaco <veronique.jollivel@wanadoo.fr>
2017-04-08 18:50:08 +0200

    quete 2

---
+++ src/WCS/CoavBundle/Entity/Reservation.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace WCS\CoavBundle\Entity;
+
+/**
+ * Reservation
+ */
+class Reservation
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var int
+     */
+    private $nbSeats;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Reservation
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set nbSeats
+     *
+     * @param integer $nbSeats
+     *
+     * @return Reservation
+     */
+    public function setNbSeats($nbSeats)
+    {
+        $this->nbSeats = $nbSeats;
+
+        return $this;
+    }
+
+    /**
+     * Get nbSeats
+     *
+     * @return int
+     */
+    public function getNbSeats()
+    {
+        return $this->nbSeats;
+    }
+}
+
